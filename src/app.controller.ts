20b5e5f060849f0360714fc7e9b2458d712c84a7
cayo <cayofonseca12@gmail.com>
2026-06-24 16:10:59 -0300

    docs: instala swagger para produção da documentação
    -Configura versionamento da API
    
    -Instala biome para configuração de linter e formatação do código

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({ version: '1' })
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Health check successfull',
+    schema: {
+      type: 'object',
+      properties: {
+        message: {
+          type: 'string',
+        },
+      },
+    },
+  })
+  getHealthCheck(): { message: string } {
+    return this.appService.getHealthCheck();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Tasks API')
+    .setDescription('API desenvolvida durante o curso de NestJs do Monaro')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
